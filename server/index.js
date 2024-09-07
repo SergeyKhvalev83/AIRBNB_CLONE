cc04288d24861a8964fef7f224fbab1c488a365c
SergeyKhvalev83 <khvalevelbrus@gmail.com>
2024-09-07 18:05:38 -0400

    Change build folder to dist because of vite

--- server/index.js
+++ server/index.js
@@ -21,13 +21,23 @@ if (port == null || port == '') {
   port = 8000;
 }
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use('/', express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//   });
+// }
+
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
+  app.use('/', express.static('dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
   });
 }
+
+
 
 app.use(helmet.contentSecurityPolicy({
   directives: {
